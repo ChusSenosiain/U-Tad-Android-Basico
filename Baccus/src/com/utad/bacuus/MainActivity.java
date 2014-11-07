5669ff7b7cdb734adc3a3cfcf8661bebf260f248
María Jesús Senosiain Caamiña <mjsenosiainc@gmail.com>
2014-11-07 19:43:24 +0100

    - Added two buttons to MainActivity of Baccus to show different wine images.

--- Baccus/src/com/utad/bacuus/MainActivity.java
+++ Baccus/src/com/utad/bacuus/MainActivity.java
@@ -4,33 +4,38 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	ImageView mWineImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mWineImage = (ImageView) findViewById(R.id.wine_image);
+         
+        Button mBtnWineBembibre = (Button) findViewById(R.id.btnWineBembibre);
+        Button mBtnWineVegaval = (Button) findViewById(R.id.btnWineVegaval);
+        
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    
+    
+    public void changeImage(View v) {
+    	
+    	if (v.getId() == R.id.btnWineVegaval) {
+    		mWineImage.setImageResource(R.drawable.vegaval);
+    	} else {
+    		mWineImage.setImageResource(R.drawable.bembibre);
+    	}
+    	
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    
+  
+   
 }
