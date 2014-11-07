8b79459b72377ef206e8e6f39bef1bb23a988de1
María Jesús Senosiain Caamiña <mjsenosiainc@gmail.com>
2014-11-07 20:51:39 +0100

    - Added LinearLayout to show the buttons in horizontal orientation.

--- Baccus/src/com/utad/bacuus/model/Grape.java
+++ Baccus/src/com/utad/bacuus/model/Grape.java
@@ -1,5 +1,26 @@
 package com.utad.bacuus.model;
 
 public class Grape {
+	
+	private String mName;
+	
+	
+	public Grape() {}
+	
+	public Grape(String name) {
+		this.mName = name;
+		
+	}
+
+	public String getName() {
+		return mName;
+	}
+
+	public void setName(String name) {
+		this.mName = name;
+	}
+	
+	
+	
 
 }
